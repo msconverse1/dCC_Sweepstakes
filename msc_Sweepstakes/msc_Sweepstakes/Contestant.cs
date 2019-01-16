736c857c663b0ed8a8669fb069f44d6f6159d38c
msconverse1 <msconverse@fullsail.edu>
2019-01-16 15:03:53 -0600

    Created Contestant Class

---
+++ msc_Sweepstakes/msc_Sweepstakes/Contestant.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace msc_Sweepstakes
+{
+    class Contestant
+    {
+        string firstName;
+        string lastName;
+        string email;
+        int registration;
+        public Contestant(string firstName,string lastName,string email,int registration)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.email = email;
+            this.registration = registration;
+        }
+    }
+}
