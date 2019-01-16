6f282679d49c16d94fbd75a9fa4c58c663358a7c
msconverse1 <msconverse@fullsail.edu>
2019-01-16 16:06:42 -0600

    Created a UserInterface to allow User to create a Contestant

---
+++ msc_Sweepstakes/msc_Sweepstakes/UserInterface.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace msc_Sweepstakes
+{
+    class UserInterface
+    {
+        Contestant sweepstakesContestant;
+        void CreateUser()
+        {
+            Console.WriteLine("Enter First Name:");
+           string firstName= Console.ReadLine();
+            Console.WriteLine("Enter Last Name:");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Enter Email Address:");
+            string email = Console.ReadLine();
+            Console.WriteLine("Enter Your Registration Number:");
+            int.TryParse(Console.ReadLine(), out int registration);
+            sweepstakesContestant = new Contestant(firstName,lastName,email,registration);
+        }
+    }
+}
