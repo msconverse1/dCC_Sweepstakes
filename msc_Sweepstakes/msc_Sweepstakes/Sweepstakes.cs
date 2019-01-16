63eb76c0084255479dff7cd8d0be3a43a27ddbe1
msconverse1 <msconverse@fullsail.edu>
2019-01-16 15:12:51 -0600

    Created empty Sweepstakes Class

---
+++ msc_Sweepstakes/msc_Sweepstakes/Sweepstakes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace msc_Sweepstakes
+{
+    class Sweepstakes
+    {
+        Sweepstakes(string name)
+        {
+
+        }
+        void RegisterContestant(Contestant contestant)
+        {
+
+        }
+        string PickWinner()
+        {
+            string temp = "placeholder";
+            return temp;
+        }
+        void PrintContestantInfo(Contestant contestant)
+        {
+
+        }
+    }
+}
