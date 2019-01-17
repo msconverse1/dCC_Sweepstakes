ab6c065169f7abd67e3ce14bce631d0d8df8d493
msconverse1 <msconverse@fullsail.edu>
2019-01-17 14:06:59 -0600

    Changed RegistrationGenerator to O(N)

--- msc_Sweepstakes/msc_Sweepstakes/UserInterface.cs
+++ msc_Sweepstakes/msc_Sweepstakes/UserInterface.cs
@@ -12,22 +12,7 @@ namespace msc_Sweepstakes
        static public Contestant CreateUser( Dictionary<int, Contestant> dictionary,Random random)
         {
             Contestant sweepstakesContestant;
-            int number;
-            int maxvalue = 4;
-            do
-            {
-                if (dictionary.Count == maxvalue)
-                {
-                    maxvalue *= maxvalue;
-                }
-                if (dictionary.Keys.Count == 0)
-                {
-                   
-                    number = random.Next(maxvalue); 
-                }
-                else
-                number = random.Next(dictionary.Keys.Min(), maxvalue);
-            } while (dictionary.Keys.Contains(number));
+            int number = RegistrationGenerator(dictionary,random);
             Console.WriteLine("Enter First Name:");
            string firstName= Console.ReadLine();
             Console.WriteLine("Enter Last Name:");
@@ -36,6 +21,21 @@ namespace msc_Sweepstakes
             string email = Console.ReadLine();
             Console.WriteLine("Your Registration Number: {0}", number);
           return sweepstakesContestant = new Contestant(firstName,lastName,email, number);
+        }
+
+        private static int RegistrationGenerator(Dictionary<int, Contestant> dictionary, Random random)
+        {
+            int number;
+            int maxvalue = 4;
+            do
+            {
+                if (dictionary.Count == maxvalue)
+                {
+                    maxvalue *= maxvalue;
+                }
+                number = random.Next(maxvalue);    
+            } while (dictionary.Keys.Contains(number));
+            return number;
         }
     }
 }
